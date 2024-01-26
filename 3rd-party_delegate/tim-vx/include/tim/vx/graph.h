113a9cc6dac437a729683a47a5e58da4976e2b79
junyi.shen <475833842@qq.com>
2024-01-26 15:49:19 +0800

    3rd-party-delegate first release

---
+++ 3rd-party_delegate/tim-vx/include/tim/vx/graph.h
@@ -0,0 +1,108 @@
+/****************************************************************************
+*
+*    Copyright (c) 2020-2023 Vivante Corporation
+*
+*    Permission is hereby granted, free of charge, to any person obtaining a
+*    copy of this software and associated documentation files (the "Software"),
+*    to deal in the Software without restriction, including without limitation
+*    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+*    and/or sell copies of the Software, and to permit persons to whom the
+*    Software is furnished to do so, subject to the following conditions:
+*
+*    The above copyright notice and this permission notice shall be included in
+*    all copies or substantial portions of the Software.
+*
+*    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+*    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+*    DEALINGS IN THE SOFTWARE.
+*
+*****************************************************************************/
+#ifndef TIM_VX_GRAPH_H_
+#define TIM_VX_GRAPH_H_
+
+#ifdef BUILD_WITH_BAZEL
+#include "vsi_feat_ops_def.h"
+#endif
+#ifdef ENABLE_TENSOR_CACHE
+#include <openssl/evp.h>
+#include <string>
+#endif
+#include <memory>
+#include <vector>
+namespace tim {
+namespace vx {
+#ifdef ENABLE_TENSOR_CACHE
+const std::string calculateMd5Secret32(const std::string& src);
+#endif
+class Tensor;
+struct TensorSpec;
+struct DmaBufferDesc;
+class Operation;
+
+class Graph {
+ public:
+  virtual ~Graph() {}
+
+  /// Create a tensor with given `TensorSpec`
+  virtual std::shared_ptr<Tensor> CreateTensor(const TensorSpec& spec,
+                                               const void* data = nullptr) = 0;
+
+  virtual std::shared_ptr<Tensor> CreateTensor(const TensorSpec& spec,
+                                               const DmaBufferDesc& dmafd) = 0;
+
+  /// Create a tensor with given `TensorSpec`.
+  /// spec.attr_ must be TensorAttribute::Input or Output
+  virtual std::shared_ptr<Tensor> CreateIOTensor(const TensorSpec& spec,
+                                               void* data = nullptr) = 0;
+
+  /// Create a placeholder tensor for optional inputs of operations
+  virtual std::shared_ptr<Tensor> CreateTensorPlaceHolder() = 0;
+
+  /// Freeze graph
+  virtual bool Compile() = 0;
+
+  /// Compile to BinaryGraph
+  virtual bool CompileToBinary(void* buf, size_t* size) = 0;
+
+  virtual bool Run() = 0;
+
+  template <typename OpType, typename... Params>
+  std::shared_ptr<OpType> CreateOperation(Params... parameters) {
+    auto op = std::make_shared<OpType>(this, parameters...);
+    op_vector_.push_back(op);
+    return op;
+  }
+
+  virtual const std::vector<std::shared_ptr<Tensor>> InputsTensor() const = 0;
+  virtual const std::vector<std::shared_ptr<Tensor>> OutputsTensor() const = 0;
+
+  virtual void UpdateTensorConsumersMap(
+      const std::shared_ptr<Tensor>& tensor,
+      const Operation* op) = 0;
+
+  virtual void UpdateTensorProducerMap(
+      const std::shared_ptr<Tensor>& tensor,
+      const Operation* op) = 0;
+
+  virtual const std::vector<std::shared_ptr<Operation>> GetConsumersOp(
+      std::shared_ptr<Tensor> tensor) const = 0;
+
+  virtual std::shared_ptr<Operation> GetProducerOp(
+      std::shared_ptr<Tensor> tensor) = 0;
+
+  virtual void PrintGraph() const = 0;
+
+  const std::vector<std::shared_ptr<Tensor>> GetConstantInputs() const;
+
+ protected:
+  std::vector<std::shared_ptr<tim::vx::Operation>> op_vector_;
+};
+
+}  // namespace vx
+}  // namespace tim
+
+#endif /* TIM_VX_GRAPH_H_ */
---
+++ 3rd-party_delegate/tim-vx/include/tim/vx/ops/deconv.h
@@ -0,0 +1,92 @@
+/****************************************************************************
+*
+*    Copyright (c) 2020-2023 Vivante Corporation
+*
+*    Permission is hereby granted, free of charge, to any person obtaining a
+*    copy of this software and associated documentation files (the "Software"),
+*    to deal in the Software without restriction, including without limitation
+*    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+*    and/or sell copies of the Software, and to permit persons to whom the
+*    Software is furnished to do so, subject to the following conditions:
+*
+*    The above copyright notice and this permission notice shall be included in
+*    all copies or substantial portions of the Software.
+*
+*    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+*    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+*    DEALINGS IN THE SOFTWARE.
+*
+*****************************************************************************/
+#ifndef TIM_VX_OPS_DECONV_H_
+#define TIM_VX_OPS_DECONV_H_
+
+#include <array>
+
+#include "tim/vx/builtin_op.h"
+
+namespace tim {
+namespace vx {
+namespace ops {
+
+/**
+ * ## DeConv2d
+ *
+ * Performs the transpose of 2-D convolution operation.
+ *
+ * This operation is sometimes called "deconvolution" after Deconvolutional Networks,
+ * but is actually the transpose (gradient) of Conv2D rather than an actual deconvolution.
+ *
+ * - oc_count_ : the out channel count for weight tensor.
+ * - pad_type : SAME, VALID or AUTO.
+ * - ksize : the height and width for weight tensor.
+ * - padding : AUTO, VALID or SAME.
+ * - pad : pad value for each spatial axis.
+ * - stride : stride along each spatial axis.
+ * - output_padding : specifying the amount of padding along the height and width of
+ * the output tensor.
+ * - group : the feature count of each group.
+ * - input_layout : Layout for input, WHCN by default.
+ * - kernel_layout: Layout for kernel, WHIO by default.
+ */
+
+class DeConv2d : public BuiltinOp {
+  public:
+    DeConv2d(Graph* graph, int32_t oc_count_, PadType pad_type,
+        const std::array<uint32_t, 2>& ksize,
+        const std::array<uint32_t, 2>& stride,
+        const std::array<uint32_t, 2>& output_padding,
+        DataLayout input_layout = DataLayout::WHCN,
+        DataLayout kernel_layout = DataLayout::WHIcOc);
+    DeConv2d(Graph* graph, int32_t oc_count_, PadType pad_type,
+        const std::array<uint32_t, 2>& ksize,
+        const std::array<uint32_t, 2>& stride,
+        const std::array<uint32_t, 2>& output_padding,
+        const std::array<uint32_t, 4>& pad,
+        const uint32_t group = 1,
+        DataLayout input_layout = DataLayout::WHCN,
+        DataLayout kernel_layout = DataLayout::WHIcOc);
+
+    DataLayout KernelDataLayout() { return kernel_layout_; }
+  
+    std::shared_ptr<Operation> Clone(std::shared_ptr<Graph>& graph) const override;
+
+  protected:
+    const uint32_t oc_count_;
+    const PadType pad_type_;
+    const std::array<uint32_t, 2> ksize_;
+    const std::array<uint32_t, 2> stride_;
+    const std::array<uint32_t, 2> output_padding_;
+    const std::array<uint32_t, 4> pad_;
+    const uint32_t group_;
+    const DataLayout kernel_layout_;
+};
+
+} // namespace ops
+} // namespace vx
+} // namespace tim
+
+#endif /* TIM_VX_OPS_DECONV_H_ */
